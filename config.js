df36c46c939e51c928f84445c89b849ee6bde3e6
Nathan Newman <lordnate.moose@gmail.com>
2023-01-14 19:28:57 -0600

    passed tests

--- config.js
+++ config.js
@@ -4,14 +4,18 @@
 
 require("dotenv").config();
 
-const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+const DB_URI =
+  process.env.NODE_ENV === "test"
+    ? "postgresql:///messagely_test"
+    : "postgresql:///messagely";
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
 const BCRYPT_WORK_FACTOR = 12;
 
+// const SECRET_KEY = process.env.SECRET_KEY;
+
+// const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR;
 
 module.exports = {
   DB_URI,
